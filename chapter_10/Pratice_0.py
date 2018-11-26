d1fadf7f299dc0e08b4c881c2c074b7f0d9a4657
CHUNGGAK LEE <leethomas@outlook.kr>
2018-11-26 15:53:29 +0900

    update chapter10 Practice0

--- chapter_10/Pratice_0.py
+++ chapter_10/Pratice_0.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import Perceptron
 import tensorflow as tf
+from sklearn.metrics import accuracy_score
 
 iris = load_iris()
 X = iris.data[:, (2, 3)]
@@ -12,7 +13,70 @@ per_clf.fit(X, y)
 
 y_pred = per_clf.predict([[2, 0.5]])
 
-feature_cols = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)
+feature_cols = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)  # infer_real_valued_columns_from_input()
+# 함수는 tf.contrib.learn 하위의 DNNClassifier, DNNRegressor, LinearClassifier, LinearRegressor 등의 클래스에 정수 또는
+# 실수로 된 특성을 매핑하기 위한 FeatureColumn 객체를 만든다. 범주형 데이터의 경우 sparse_column_with_key() 함수 등을
+# 사용할 수 있다.
 dnn_clf = tf.estimator.DNNClassifier(hidden_units=[300, 100], n_classes=10, feature_columns=feature_cols)
 dnn_clf = tf.contrib.learn.SKCompat(dnn_clf)
 dnn_clf.fit(X_train, y_train, batch_size=50, steps=40000)
+
+y_pred = dnn_clf.predict(X_test)
+accuracy_score(y_test, y_pred['classes'])
+
+n_inputs = 28*28
+n_hidden1 = 300
+n_hidden2 = 100
+n_outpus = 10
+
+X = tf.placeholder(tf.float, shape=(None, n_inputs), name='X')
+y = tf.placeholder(tf.int32, shape=(None), name='y')
+
+def neuron_layer(X, n_neurons, name, activation=None):  # 매개변수: 입력, 뉴런 수, 층 이름, 활성화 함수
+    with tf.name_scope(name):  # 층 이름으로 이름 범위를 만듦. 여기에 이 층에서 필요한 모든 계산 노드가 포함된다
+        n_inputs = int(X.get_shape()[1])  # 입력 특성의 수 구하기
+        stddev = 2 / np.sqrt(n_inputs + n_neurons)
+        init = tf.truncated_normal((n_inputs, n_neurons), stddev=stddev)
+        W = tf.Variable(init, name='kernel')  # 가중치 행렬을 담을 W 변수 만듦기(커널). 이 행렬은 각 입력과 각 뉴런
+        # 사이의 모든 연결 가중치를 담고 있는 2D 텐서. 크기는 (n_inputs, n_neurons). 이 행렬은 표준편차가
+        # 2 / (n_inputs + n_neurons)^(1/2)인 절단 정규(가우시안) 분포(truncated normal distribution)를 사용해 무작위로
+        # 초기화. 이 표준편차를 사용하면 알고리즘이 빠르게 수렴(이것은 신경망을 엄청나게 효율적으로 만들어준 변화).
+        # 경사 하강법 알고리즘이 중단되지 않도록 대칭성을 피하기 위해 모든 은닉층의 가중치는 무작위로 초기화하는 것이
+        # 중요.
+        b = tf.Variable(tf.zeros([n_neurons]), name='bias') # 뉴런마다 하나의 편향을 갖도록 변수 b를 만들고 0으로 초기화
+        # (여기서는 대칭 문제가 없다)
+        Z = tf.matmul(X, W) + b  #층에 있는 모든 뉴런과 배치에 있는 모든 샘플에 대해 입력에 대한 가중치 합에 편향을
+        # 더하는 계산을 수행. 브로드캐스팅.
+        if activation is not None:  # tf.nn.relu와 같은 activation 매개변수가 지정되어 있지 않으면 activation(Z)(즉
+            # max(0, Z))를 반환, 그렇지 않으면 Z 반환
+            return activation(Z)
+        else:
+            return Z
+
+with tf.name_scope('dnn'):
+    hidden1 = neuron_layer(X, n_hidden1, name='hidden1', activation='tf.nn.relu')
+    hidden2 = neuron_layer(hidden1, n_hidden2, name='hidden2', activation='tf.nn.relu')
+    logits = neuron_layer(hidden2, n_outputs, name='outputs')  # 소프트맥스 활성화 함수로 들어가기 직전의 신경망 출력
+
+with tf.name_scope('dnn'):
+    hidden1 = tf.layers.dense(X, n_hidden1, name='hidden1', activation=tf.nn.relu)
+    hidden2 = tf.layers.dense(hidden1, n_hidden2, name='hidden2', activation=tf.nn.relu)
+    logits = tf.layers.dense(hidden2, n_outputs, name='outputs')
+
+with tf.name_scope('loss'):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy)
+
+learning_rate = 0.01
+
+with tf.name_scope('train'):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+with. tf.name_scope('eval'):
+    correct = tf.nn.in_top_k(logits, y, 1)  # in_top_k(predictions, targets, k): predictions과 targets을 입력받아
+    # 타깃 레이블의 예측값이 크기순으로 k 번째 안에 들면 True, 그렇지 않으면 False를 반환.
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train_Saver()
